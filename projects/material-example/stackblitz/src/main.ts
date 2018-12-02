9dbb819accd077578ae40a3219702d4d651021a9
Guenter Sandner <www.gms@gmx.at>
2018-12-02 11:18:29 +0100

    build: update stackblitz example

--- projects/material-example/stackblitz/src/main.ts
+++ projects/material-example/stackblitz/src/main.ts
@@ -1,7 +1,5 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
